7be4c1ea4de50a439c1818d0a6c063b22474c983
moritalous <moritalous@gmail.com>
2016-08-18 15:25:15 +0900

    Commit

--- js/ymap.js
+++ js/ymap.js
@@ -1,193 +1,118 @@
-var pokeList = {
-  '35.66582': {
-    '139.7311': {
-      'name': 'ヤヤコマ',
-      'img': 'ff08ec6198db300abc91e69605469427.png'
-    }
-  }
-};
-
-var findedPokeList = {
-  '35.66592': {
-    '139.7321': {
-      'name': 'ピカチュウ',
-      'img': 'e5be30e8a3f8dba0657a9e8220aaae30.png'
-    }
-  }
-};
+var unfindedPokeList;
+var findedPokeList;
 
 var ymap = new Y.Map("map");
 
 window.onload = function() {
-
-
-  ymap.drawMap(new Y.LatLng(35.66572, 139.73100), 17, Y.LayerSetId.NORMAL);
-
-  // ズームしちゃうと、画像を小さくしないといけない
-  // var control = new Y.SliderZoomControlVertical();
-  // ymap.addControl(control);
-
-  // 未発見ポケモンの表示
-  for (var pokeLat in pokeList) {
-    for (var pokeLng in pokeList[pokeLat]) {
-      var label = new Y.Label(new Y.LatLng(pokeLat, pokeLng), "ぽけもん!!");
-      label.bind('click', function(latlng) {
-
-        var poke = pokeList[this.latlng.Lat][this.latlng.Lon];
-        var name = poke['name'];
-        var img = poke['img'];
-
-        // /////
-        // //キーボード操作などにより、オーバーレイが多重起動するのを防止する
-        // $(this).blur(); //ボタンからフォーカスを外す
-        // if ($("#modal-overlay")[0]) return false; //新しくモーダルウィンドウを起動しない (防止策1)
-        // //if($("#modal-overlay")[0]) $("#modal-overlay").remove() ;		//現在のモーダルウィンドウを削除して新しく起動する (防止策2)
-        //
-        // //オーバーレイを出現させる
-        // $("body").append('<div id="modal-overlay"></div>');
-        // $("#modal-overlay").fadeIn("slow");
-        //
-        // //コンテンツをセンタリングする
-        // //画面(ウィンドウ)の幅、高さを取得
-        // var w = $(window).width();
-        // var h = $(window).height();
-        //
-        // // コンテンツ(#modal-content)の幅、高さを取得
-        // // jQueryのバージョンによっては、引数[{margin:true}]を指定した時、不具合を起こします。
-        // //		var cw = $( "#modal-content" ).outerWidth( {margin:true} );
-        // //		var ch = $( "#modal-content" ).outerHeight( {margin:true} );
-        // var cw = $("#modal-content").outerWidth();
-        // var ch = $("#modal-content").outerHeight();
-        //
-        // //センタリングを実行する
-        // $("#modal-content").css({
-        //   "left": ((w - cw) / 2) + "px",
-        //   "top": ((h - ch) / 2) + "px"
-        // });
-        //
-        // $("#modal-content").append('<div id="modal-poke-text">' + name + 'をみつけた！！</div>');
-        // $("#modal-content").append('<div id="modal-poke-image"><img src="http://www.pokemon.jp/zukan/images/m/' + img + '"></div>');
-        //
-        // //コンテンツをフェードインする
-        // $("#modal-content").fadeIn("slow");
-        //
-        // //[#modal-overlay]、または[#modal-close]をクリックしたら…
-        // $("#modal-overlay,#modal-close").unbind().click(function() {
-        //
-        //   //[#modal-content]と[#modal-overlay]をフェードアウトした後に…
-        //   $("#modal-content,#modal-overlay").fadeOut("slow", function() {
-        //
-        //     //[#modal-overlay]を削除する
-        //     $('#modal-overlay').remove();
-        //     $('#modal-poke-text').remove();
-        //     $('#modal-poke-image').remove();
-        //
-        //   });
-        //
-        // });
-        // /////
-
-        var icon = new Y.Icon('http://www.pokemon.jp/zukan/images/m/' + img);
-        var marker = new Y.Marker(new Y.LatLng(this.latlng.Lat, this.latlng.Lon), {
-          icon: icon
-        });
-        ymap.addFeature(marker);
-
-        ymap.removeFeature(this);
-        ymap.addFeature(new Y.Label(new Y.LatLng(this.latlng.Lat, this.latlng.Lon), name));
-
-        $("#finded-pokemon").append('<div id="modal-poke-text" onclick="movepoint('+this.latlng.Lat+', '+this.latlng.Lon+')"><a class="button-link">' + name + '</a></div>');
-
-      });
-      ymap.addFeature(label);
-    }
-  }
-
-  // 発見済みポケモンの表示
-  for (var pokeLat in findedPokeList) {
-    for (var pokeLng in findedPokeList[pokeLat]) {
-      var poke = findedPokeList[pokeLat][pokeLng];
-      var name = poke['name'];
-      var img = poke['img'];
-
-      var label = new Y.Label(new Y.LatLng(pokeLat, pokeLng), name);
-      ymap.addFeature(label);
-
-      var icon = new Y.Icon('http://www.pokemon.jp/zukan/images/m/' + img);
-      var marker = new Y.Marker(new Y.LatLng(pokeLat, pokeLng), {
-        icon: icon
-      });
-      ymap.addFeature(marker);
-
-      $("#finded-pokemon").append('<div id="modal-poke-text" onclick="movepoint('+pokeLat+', '+pokeLng+')"><a class="button-link">' + name + '</a></div>');
-
-    }
-  }
-
+  initMap();
+  requestUnfinded();
 }
-
-// $(function() {
-//
-//   //モーダルウィンドウを出現させるクリックイベント
-//   $("#modal-open").click(function() {
-//
-//     //キーボード操作などにより、オーバーレイが多重起動するのを防止する
-//     $(this).blur(); //ボタンからフォーカスを外す
-//     if ($("#modal-overlay")[0]) return false; //新しくモーダルウィンドウを起動しない (防止策1)
-//     //if($("#modal-overlay")[0]) $("#modal-overlay").remove() ;		//現在のモーダルウィンドウを削除して新しく起動する (防止策2)
-//
-//     //オーバーレイを出現させる
-//     $("body").append('<div id="modal-overlay"></div>');
-//     $("#modal-overlay").fadeIn("slow");
-//
-//     //コンテンツをセンタリングする
-//     centeringModalSyncer();
-//
-//     //コンテンツをフェードインする
-//     $("#modal-content").fadeIn("slow");
-//
-//     //[#modal-overlay]、または[#modal-close]をクリックしたら…
-//     $("#modal-overlay,#modal-close").unbind().click(function() {
-//
-//       //[#modal-content]と[#modal-overlay]をフェードアウトした後に…
-//       $("#modal-content,#modal-overlay").fadeOut("slow", function() {
-//
-//         //[#modal-overlay]を削除する
-//         $('#modal-overlay').remove();
-//
-//       });
-//
-//     });
-//
-//   });
-//
-//   //リサイズされたら、センタリングをする関数[centeringModalSyncer()]を実行する
-//   $(window).resize(centeringModalSyncer);
-//
-//   //センタリングを実行する関数
-//   function centeringModalSyncer() {
-//
-//     //画面(ウィンドウ)の幅、高さを取得
-//     var w = $(window).width();
-//     var h = $(window).height();
-//
-//     // コンテンツ(#modal-content)の幅、高さを取得
-//     // jQueryのバージョンによっては、引数[{margin:true}]を指定した時、不具合を起こします。
-//     //		var cw = $( "#modal-content" ).outerWidth( {margin:true} );
-//     //		var ch = $( "#modal-content" ).outerHeight( {margin:true} );
-//     var cw = $("#modal-content").outerWidth();
-//     var ch = $("#modal-content").outerHeight();
-//
-//     //センタリングを実行する
-//     $("#modal-content").css({
-//       "left": ((w - cw) / 2) + "px",
-//       "top": ((h - ch) / 2) + "px"
-//     });
-//   }
-// });
 
 function movepoint(lat, lng) {
   // console.log(lat)
   // console.log(lng)
   ymap.panTo(new Y.LatLng(lat, lng), true);
+}
+
+function requestUpdate(pokemon) {
+  pokemon['finded'] = 'true'
+  console.log(pokemon);
+
+  $.ajax({
+    type: 'POST',
+    url: 'https://9l80qquq46.execute-api.us-east-1.amazonaws.com/prod/PokeMapFunction',
+    headers: {
+      "Origin": "http://localhost:8080",
+      "Access-Control-Allow-Origin" : ""
+    },
+    data: JSON.stringify(pokemon),
+  });
+}
+
+function requestUnfinded() {
+  // 未発見ポケモン
+  $.ajax({
+    type: 'GET',
+    url: 'https://9l80qquq46.execute-api.us-east-1.amazonaws.com/prod/PokeMapGet?finded=false',
+    dataType: 'jsonp',
+    contentType: 'application/json',
+    jsonpCallback: 'callback',
+    success: function(json) {
+      console.log(json);
+      unfindedPokeList = json.Items;
+      setUnfindedPokemon();
+      requestFinded();
+    }
+  });
+}
+
+function requestFinded() {
+  // 発見済みポケモン
+  $.ajax({
+    type: 'GET',
+    url: 'https://9l80qquq46.execute-api.us-east-1.amazonaws.com/prod/PokeMapGet?finded=true',
+    dataType: 'jsonp',
+    contentType: 'application/json',
+    jsonpCallback: 'callback',
+    success: function(json) {
+      console.log(json);
+      findedPokeList = json.Items;
+      setFindedPokemon();
+    }
+  });
+}
+
+
+
+
+function setFindedPokemon() {
+  for (var no in findedPokeList) {
+    var pokemon = findedPokeList[no];
+    console.log(pokemon);
+
+    var label = new Y.Label(new Y.LatLng(parseFloat(pokemon['lat']), parseFloat(pokemon['lng'])), pokemon['name']);
+    ymap.addFeature(label);
+
+    var icon = new Y.Icon('http://www.pokemon.jp/zukan/images/m/' + pokemon['img']);
+    var marker = new Y.Marker(new Y.LatLng(parseFloat(pokemon['lat']), parseFloat(pokemon['lng'])), {
+      icon: icon
+    });
+    ymap.addFeature(marker);
+
+    $("#finded-pokemon").append('<div id="modal-poke-text" onclick="movepoint(' + pokemon['lat'] + ', ' + pokemon['lng'] + ')"><a class="button-link">' + pokemon['name'] + '</a></div>');
+
+  }
+}
+
+function setUnfindedPokemon() {
+  for (var no in unfindedPokeList) {
+    var pokemon = unfindedPokeList[no];
+    console.log(pokemon);
+
+    var label = new Y.Label(new Y.LatLng(parseFloat(pokemon['lat']), parseFloat(pokemon['lng'])), pokemon['station']);
+    label.pokemon = pokemon;
+    label.bind('click', function(latlng) {
+      console.log(latlng);
+      console.log(this.pokemon);
+
+      var icon = new Y.Icon('http://www.pokemon.jp/zukan/images/m/' + this.pokemon['img']);
+      var marker = new Y.Marker(new Y.LatLng(this.latlng.Lat, this.latlng.Lon), {
+        icon: icon
+      });
+      ymap.addFeature(marker);
+
+      ymap.removeFeature(this);
+      ymap.addFeature(new Y.Label(new Y.LatLng(this.latlng.Lat, this.latlng.Lon), this.pokemon['name']));
+
+      $("#finded-pokemon").append('<div id="modal-poke-text" onclick="movepoint(' + this.latlng.Lat + ', ' + this.latlng.Lon + ')"><a class="button-link">' + this.pokemon['name'] + '</a></div>');
+
+      // 発見済みの更新
+      requestUpdate(this.pokemon);
+    });
+    ymap.addFeature(label);
+  }
+}
+
+function initMap() {
+  ymap.drawMap(new Y.LatLng(35.66572, 139.73100), 17, Y.LayerSetId.NORMAL);
 }
